eef9db21c935c9c3a78af777722d0a66d5a183a1
AitorDuran <aitorduranmonge@gmail.com>
2016-06-08 13:41:17 +0200

    cambios

--- test.sh
+++ test.sh
@@ -3,10 +3,10 @@
 testing=$( curl http://web:80 | grep "This is Phantom, a free, fully responsive site" | wc -l )
 
 if [ $testing -eq 1 ]; then
-	echo "El test ha sido finalizado correctamente" 1>&2
+	echo "El test ha sido finalizado correctamente" 
 	exit 0
 
-elif [ $testing -eq 0 ]; then
- 	echo "El test no ha realizado las pruebas correctamente" 1>&2
+else
+ 	echo "El test no ha realizado las pruebas correctamente" 
  	exit 1
 fi
